fbf1390c38fdd717990201becfe51688091e67a3
Jokester4690 <Jokester4690@gmail.com>
2016-12-11 16:02:08 -0600

    Enchants!

--- src/JungleRealm/EventListener.php
+++ src/JungleRealm/EventListener.php
@@ -2,10 +2,21 @@
 
 namespace EventListener;
 
+/*
+Custom Enchants Key
+§c = Common
+§e = Unique
+§a = Legendary
+§5 = Mythical
+*/
+
 use onebone\economyapi\EconomyAPI;
 use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\{Player, Server};
 use pocketmine\utils\TextFormat as C;
+use pocketmine\item\Armor;
+use pocketmine\item\Item;
+use pocketmine\item\enchantment\Enchantment;
 
 class EventListener implements Listener{
   public function __construct(EventListener $plugin){
@@ -19,23 +30,44 @@ class EventListener implements Listener{
         if($cause == EntityDamageEvent::CAUSE_FALL){
           $event->setCancelled(true);
         }
+        
         $p = $event->getEntity();
         $pl = $event->getDamager();
         $item = $pl->getInventory()->getItemInHand();
-        if(!$item->hasEnchantments()) return;
-        if($item->hasEnchantment(100)){
-          $this->ce->callEnchant("Blindness", $pl, $p, $item->getEnchantment(100)->getLevel(), $event);
-        }
-          if($it->hasEnchantment(101)){ 
-            $this->ce->callEnchant("Venom", $pl, $p, $item->getEnchantment(101)->getLevel(), $event);
-          }
-            if($it->hasEnchantment(102)){
-              $this->ce->callEnchant("Vampire", $pl, $p, $item->getEnchantment(102)->getLevel(), $event);
-            }
-              if($it->hasEnchantment(103)){
-                $this->ce->callEnchant("Obliteration", $pl, $p, $item->getEnchantment(103)->getLevel(), $event);
-              }
-                 if($it->hasEnchantment(104)){
-                   $this->ce->callEnchant("Wither", $pl, $p, $item->getEnchantment(119)->getLevel(), $event);
-                 }
+        $armor = $p->getInventory()->getArmorContents();
         
+        if(!$item->hasEnchantments()){
+          return;
+        if($item->hasEnchantment(100)){
+          $this->ce->callEnchant("§cBlindness", $pl, $p, $item->getEnchantment(100)->getLevel(), $event);
+        }
+          if($item->hasEnchantment(101)){ 
+            $this->ce->callEnchant("§cVenom", $pl, $p, $item->getEnchantment(101)->getLevel(), $event);
+          }
+            if($item->hasEnchantment(102)){
+              $this->ce->callEnchant("§5Vampire", $pl, $p, $item->getEnchantment(102)->getLevel(), $event);
+            }
+              if($item->hasEnchantment(103)){
+                $this->ce->callEnchant("§eObliteration", $pl, $p, $item->getEnchantment(103)->getLevel(), $event);
+              }
+                 if($item->hasEnchantment(104)){
+                   $this->ce->callEnchant("§eWither", $pl, $p, $item->getEnchantment(104)->getLevel(), $event);
+                 }
+                    if($item->hasEnchantment(105)){
+                      $this->ce->callEnchant("§eIceAspect", $pl, $p, $item->getEnchantment(105)->getLevel(), $event);
+                    }
+                       if($item->hasEnchantment(106)){
+                         $this->ce->callEnchant("§aFurnace", $pl, $p, $item->getEnchantment(106)->getLevel(), $event);
+                       }
+                          if($item->hasEnchantment(107)){
+                            $this->ce->callEnchant("§aCloak", $pl, $p, $item->getEnchantment(107)->getLevel(), $event);
+                          }
+                             if($armor->hasEnchantment(108)){
+                               $this->ce->callEnchant("§aSwift", $pl, $p, $armor->getEnchantment(108)->getLevel(), $event);
+                             }
+                                if($armor->hasEnchantment(109)){
+                                  $this->ce->callEnchant("§aBunny", $pl, $p, $armor->getEnchantment(109)->getLevel(), $event);
+                                }
+                                   if($armor->hasEnchantment(110)){
+                                     $this->ce->callEnchant("§5Escape", $pl, $p, $armor->getEnchantment(110)->getLevel(), $event);
+                                   }
