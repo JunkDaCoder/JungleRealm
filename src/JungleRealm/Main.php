e969bc89f18130adf7bd0009efea27a1a050a1e9
Jokester4690 <Jokester4690@gmail.com>
2016-12-10 22:57:46 -0600

    A lot

--- src/JungleRealm/Main.php
+++ src/JungleRealm/Main.php
@@ -13,8 +13,13 @@ use pocketmine\math\Vector3;
 use pocketmine\level\particle\FloatingTextParticle;
 use pocketmine\level\sound\ButtonClickSound;
 use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\item\enchantment\Enchantment
 
 class Main extends PluginBase implements Listener{
+  
+  public $interval = 10;
+  public $players;
+    
   public function onEnable(){
     new Config($this->getDataFolder() . "config.yml", Config::YAML);
     $this->saveResource("config.yml");
@@ -56,3 +61,40 @@ class Main extends PluginBase implements Listener{
       if($killer instanceof Player){
         $this->getServer()->broadcastMessage(C::DARK_RED . C::BOLD . "x " . C::RESET . C::YELLOW . $victimname . C::ORANGE . " killed by " . C::YELLOW . $killername . C::ORANGE . " with " . C::AQUA . $weapon . C::DARK_RED . C::BOLD . " x");
       }
+      
+      public function enterCombat(Player $player){
+        if(isset($this->players[$player->getName()])){
+          if((time() - $this->players[$player->getName()]) > $this->interval){
+            $player->sendMessage(C::DARK_GRAY . C::BOLD . "[" . C::RED . "COMBAT" . C::DARK_GRAY . "]" . C::RESET . C::GRAY . " You've entered combat! Don't log out for " . C::YELLOW . "10 SECONDS" . C::GRAY . "!");
+          }
+        }else{
+          $player->sendMessage(C::DARK_GRAY . C::BOLD . "[" . C::RED . "COMBAT" . C::DARK_GRAY . "]" . C::RESET . C::GRAY . " You've entered combat! Don't log out for " . C::YELLOW . "10 SECONDS" . C::GRAY . "!");
+        }
+        $this->players[$player->getName()] = time();
+      }
+      public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+        switch (strtolower($cmd->getName())){
+          case "me":
+            $sender->sendMessage(C::DARK_GRAY . C::BOLD . "[" . C::RED . "COMMANDS" . C::DARK_GRAY . "]" . C::RESET . C::YELLOW . " This command is blocked!");
+            break;
+          case "list":
+            
+            $online = "";
+            $onlineCount = 0;
+            
+            foreach($sender->getServer()->getOnlinePlayers() as $player){
+              if($player->isOnline() and (!($sender instanceof Player))){
+                $online .= $player->getDisplayName() . ", ";
+                ++$onlineCount;
+              }
+            }
+            
+            $sender->sendMessage(C::GRAY . "There are " . C::YELLOW . $this->getServer()->getOnlinePlayers() . C::GRAY . "/ " . C::YELLOW . $this->getServer()->getMaxPlayers() . C::GRAY . " online on " . C::DARK_GREEN . "Jungle Realm" . C::GRAY . ":");
+            $sender->sendMessage(C::AQUA . substr($online, 0, -2));
+            return true;
+        }
+      }
+      break;
+    }
+  }
+}
